0bd493a0d56a3f6b1eeb4525e1ba081c23b270eb
Serge <sshibkojavadev@gmail.com>
2023-12-28 16:30:18 +0800

    FE added) react-router route to AccountList

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import AccountList from './components/accountList/AccountList';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="container-xxl">
-      <Header/>
-      <AccountList/>
-      <Footer/>
+      <BrowserRouter>
+        <Header/>
+          <Routes>
+            <Route path='api/v1/' element = {<AccountList/>}></Route>
+            <Route path='api/v1/accounts' element = {<AccountList/>}></Route>
+          </Routes>
+        <AccountList/>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
