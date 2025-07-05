2c6661bc8885534482dce71f3f4e1db0d24aab7e
Serge <sshibkojavadev@gmail.com>
2025-07-05 15:22:37 +0800

    FE: FEATURE: added deactivate for User, added sort and many other funct for UserList

--- ui/src/services/UserService.js
+++ ui/src/services/UserService.js
@@ -2,7 +2,16 @@ import axios from 'axios';
 
 const USER_REST_API_BASE_URL = "http://localhost:8088/users";
 
-export const getAllUsers = (params) => axios.get(USER_REST_API_BASE_URL, {params});
+//export const getAllUsers = (params) => axios.get(USER_REST_API_BASE_URL, {params});
+
+export const getAllUsers = (params) => {
+    return axios.get(USER_REST_API_BASE_URL, {
+        params: {
+            ...params,
+            keyword: params.keyword || '%',
+            status: params.status
+    }});
+}; 
 
 export const createUser = (user) => axios.post(USER_REST_API_BASE_URL, user);
 
@@ -13,3 +22,5 @@ export const updateUser = (userId, user) => axios.put(USER_REST_API_BASE_URL + '
 export const deleteUser = (userId) => axios.delete(USER_REST_API_BASE_URL + '/' + userId);
 
 export const findByKeyword = (keyword) => axios.get(USER_REST_API_BASE_URL + '/search/' + keyword);
+
+export const toggleStatus = (userId) => axios.patch(USER_REST_API_BASE_URL + '/' + userId + '/activate');
