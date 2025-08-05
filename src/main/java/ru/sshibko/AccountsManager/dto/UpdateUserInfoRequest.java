7c8f3fb593376877c3ecddc93a7e2c20d36ce10b
Serge <sshibkojavadev@gmail.com>
2025-08-05 11:15:10 +0800

    BE: SUPPORT: remove extended validate from userInfo nullable fields

--- src/main/java/ru/sshibko/AccountsManager/dto/UpdateUserInfoRequest.java
+++ src/main/java/ru/sshibko/AccountsManager/dto/UpdateUserInfoRequest.java
@@ -1,13 +1,9 @@
 package ru.sshibko.AccountsManager.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -16,22 +12,18 @@ import java.time.LocalDateTime;
 @Builder
 public class UpdateUserInfoRequest {
 
-    //@NotBlank
     //@Size(min = 1, max = 255)
     private String firstName;
 
-    //@NotBlank
     //@Size(min = 1, max = 255)
     private String lastName;
 
     @Past
     private LocalDate birthDate;
 
-    //@NotBlank
     //@Size(min = 1, max = 255)
     private String country;
 
-    //TODO anno filter
     private String phoneNumber;
 
     //@Email
