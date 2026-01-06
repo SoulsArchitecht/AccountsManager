248fb936456609e613bdaee22d13f01c34ceb8f3
Serge <sshibkojavadev@gmail.com>
2026-01-06 19:38:51 +0800

    FE: SUPPORT: fix homepage

--- ui/src/components/homePage/HomePage.jsx
+++ ui/src/components/homePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import bgImage from '../../assets/images/main_bg.jpg';
+//import bgImage from '../../assets/images/main_bg.jpg';
 import "./HomePage.css";
 import { useLocalization } from '../../context/LocalizationContext';
 
@@ -8,23 +8,20 @@ function HomePage() {
 
 
     return (
-        <div className="home-page">
-            <div className="home-image-container">
-                <img src={bgImage} alt="main_bg_picture" className="home-image"/>
-                <div className="home-overlay">
-                    <div className="home-content text-center text-white">
-                        <p className="home-description">{t('home.description')}</p>
-                        <a
-                        href="/swagger-ui/index.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="home-link"
-                        >
-                        {t('home.link')}
-                        </a>
-                    </div>
-                </div>
+        <div className="home-hero">
+        <div className="home-overlay">
+            <div className="home-content text-center text-white">
+            <p className="home-description">{t('home.description')}</p>
+            <a
+                href="/swagger-ui/index.html"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="home-link"
+            >
+                {t('home.link')}
+            </a>
             </div>
+        </div>
         </div>
     );
 }
